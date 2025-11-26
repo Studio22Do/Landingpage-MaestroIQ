abdffe3ce956057bb50a9b3cf966bf8bc25c3633
Ramfidelacruz <ramfi.delacruz@s22.io>
2025-11-26 10:19:44 -0400

    Add Footer component to App for enhanced layout and improved user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 import Hero from './components/sections/Hero'
 import KeyFeaturesSection from './components/sections/KeyFeaturesSection'
 import MarketingIntelligenceSection from './components/sections/MarketingIntelligenceSection'
@@ -24,6 +25,8 @@ const App = () => {
 
         <HelpSection />
       </main>
+
+      <Footer />
     </div>
   )
 }
